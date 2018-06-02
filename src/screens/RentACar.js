d0e680ed0074fcd89912ef06aa601cfa56fd36eb
edgarcheverier <edgarcheverier@gmail.com>
2018-06-02 17:47:15 +0200

    MF Commit 03

--- src/screens/RentACar.js
+++ src/screens/RentACar.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
-import { View, Text, Image, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, Button, TouchableOpacity, StyleSheet, ScrollView, Picker } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {Calendar} from 'react-native-calendars';
 
 class RentACar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDay: {},
+      selectedCity: "",
+    };
+  }
   backScreen = () => {
     this.props.navigator.push({
       screen: "welcome-screen.WelcomeScreen",
@@ -10,6 +18,14 @@ class RentACar extends Component {
       animationType: "fade",
     })
   }
+
+  onDayPress = (day) => {
+    this.setState({
+      selectedDay: day,
+      selected: day.dateString
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -19,6 +35,14 @@ class RentACar extends Component {
           </TouchableOpacity>
           <Text style={styles.navTitle}>Rent A Car Screen</Text>
         </View>
+        <ScrollView>
+        <Calendar
+          onDayPress={this.onDayPress}
+          style={styles.calendar}
+          hideExtraDays
+          markedDates={{[this.state.selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}}}
+        />
+        </ScrollView>
       </View>
     );
   }
@@ -39,7 +63,14 @@ const styles = StyleSheet.create({
   navTitle: {
     marginTop: 40,
     marginLeft: 95,
-  }
+  },
+  calendar: {
+    borderTopWidth: 1,
+    paddingTop: 5,
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+    height: 350
+  },
 })
 
 export default RentACar;
